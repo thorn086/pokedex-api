24096cbb8b616d761396a8cc061eff82b5000d5f
Darren Rose <thorn086@gmail.com>
2019-12-08 14:56:24 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('dev'))
+
+app.use((req,res)=>{
+    res 
+        .send('Hello Jaden!')
+})
+
+const PORT = 8000
+
+app.listen(PORT, ()=>{
+    console.log(`Server is listening at http://localhost:${PORT}`)
+})
+
+
