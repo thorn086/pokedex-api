dec9f691b5233a105277aa8386d085e57c87c66a
Darren Rose <thorn086@gmail.com>
2019-12-12 17:54:51 -0600

    Heroku pre-build

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
 const POKEDEX = require('./pokedex.json')
 
 const app = express()
-
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production'?'tiny':'common'
+app.use(morgan(morganSetting))
+app.use(helmet())
+app.use(cors())
 
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
     const authToken = req.get('Authorization')
-
-    if(!authToken||authToken !== apiToken){
+   
+    if(!authToken||authToken.split(' ')[1] !== apiToken){
        return res.status(401).json({error: 'Unauthorized request' })
     }
-    //move to next middleware
     next()
 })
 
@@ -29,9 +32,8 @@ app.get('/types', function handleGetTypes(req,res) {
 
 
 app.get('/pokemon',function handleGetPokemon(req,res) {
-  
     let response = POKEDEX.pokemon;
-
+    
     if(req.query.name){
         response = response.filter(pokemon => pokemon.name.toLowerCase().includes(req.query.name.toLowerCase()))
     }
@@ -42,7 +44,16 @@ app.get('/pokemon',function handleGetPokemon(req,res) {
     res.json(response)
 })
 
-const PORT = 8000
+app.use((error, req, res, next)=>{
+    let response
+    if(process.env.NODE_ENV === 'production'){
+        response = {error:{ message: 'server error'}}
+    }else{
+        response= {error}
+    }
+    res.status(500).json(response)
+})
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, ()=>{
     console.log(`Server is listening at http://localhost:${PORT}`)
